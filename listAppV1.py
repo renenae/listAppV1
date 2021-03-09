eee38ca99e35798fb491033bd4b1f8bfaee7be1b
renenae <79869201+renenae@users.noreply.github.com>
2021-03-09 11:06:50 -0800

    Add files via upload

---
+++ listAppV1.py
@@ -0,0 +1,28 @@
+"""
+
+Program Goals:
+1. Get the input from the usser!
+2. Convert that input to an INT
+3. Add that input to a list
+4. Pull values from specific index positions
+"""
+
+#create functions that will perfrom those actions above
+
+def mainProgram():
+    myList = []
+    print("Hello, there! Let's work with lists!")
+    print("Choose one of the following options. Type a number ONLY!")
+    choice = input("1. Add to list, 2. Return the value at an index position  ")
+    if choice == "1":
+        addToList()
+    elif choice == "2":
+        indexValues
+    #we need to add an exit program and error catching!
+    
+def addToList():
+
+def indexValues():
+
+if __name__ == "__main__":
+    mainProgram()
